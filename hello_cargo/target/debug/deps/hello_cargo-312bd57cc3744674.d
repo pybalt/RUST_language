afb8b8263b3b06351c9434f3ecbbf547a07b8b72
pybalt <leobravolbb2002bej@gmail.com>
2022-09-17 15:24:47 -0300

    1__ Hello Rust, Hello Cargo!

---
+++ hello_cargo/target/debug/deps/hello_cargo-312bd57cc3744674.d
@@ -0,0 +1,5 @@
+c:\Users\leobr\OneDrive\Escritorio\Home\Repositorios\RUST_language\hello_cargo\target\debug\deps\hello_cargo-312bd57cc3744674.rmeta: src\main.rs
+
+c:\Users\leobr\OneDrive\Escritorio\Home\Repositorios\RUST_language\hello_cargo\target\debug\deps\hello_cargo-312bd57cc3744674.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-eb063a268d4c4829.d
@@ -0,0 +1,5 @@
+c:\Users\leobr\OneDrive\Escritorio\Home\Repositorios\RUST_language\hello_cargo\target\debug\deps\hello_cargo-eb063a268d4c4829.rmeta: src\main.rs
+
+c:\Users\leobr\OneDrive\Escritorio\Home\Repositorios\RUST_language\hello_cargo\target\debug\deps\hello_cargo-eb063a268d4c4829.d: src\main.rs
+
+src\main.rs:
