f260ecc677868225a29fd1ece1e738a55303ba21
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2022-11-13 16:45:15 -0300

    Delete hello_cargo/target directory

--- hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\leobr\OneDrive\Escritorio\Home\Repositorios\RUST_language\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-C:\Users\leobr\OneDrive\Escritorio\Home\Repositorios\RUST_language\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-src\main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\leobr\OneDrive\Escritorio\Home\Repositorios\RUST_language\hello_cargo\target\debug\hello_cargo.exe: C:\Users\leobr\OneDrive\Escritorio\Home\Repositorios\RUST_language\hello_cargo\src\main.rs
